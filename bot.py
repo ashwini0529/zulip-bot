200c07e696ef0709ad333c4ce75fb14f5a6cc862
Ashwini Purohit <ashwinipurohit@yahoo.in>
2017-04-25 13:47:03 +0530

    Class implementation
    Message receiver yet to be implemnted for subscribed streams

--- bot.py
+++ bot.py
@@ -2,28 +2,40 @@
 import zulip
 import sys
 import env
-
-# Keyword arguments 'email' and 'api_key' are not required if you are
-# using ~/.zuliprc
-client = zulip.Client(email=env.ZULIP_EMAIL,
-                      api_key=env.ZULIP_API_KEY,
-                      site=env.ZULIP_SITE)
-# Send a stream message
-# Static message testing as of now
+import requests
 
 
-def send_message():
-    client.send_message({
-        "type": "stream",
-        "to": "bot-testing",
-        "subject": "coala loves python",
-        "content": "Message is sent successfully!."
-    })
-# Print each message the user receives
-# This is a blocking call that will run forever
-client.call_on_each_message(lambda msg: sys.stdout.write(str(msg) + "\n"))
+class cobotZulip():
 
-# Print every event relevant to the user
-# This is a blocking call that will run forever
-# This will never be reached unless you comment out the previous line
-client.call_on_each_event(lambda msg: sys.stdout.write(str(msg) + "\n"))
+    def __init__(self, zulip_api, zulip_email, zulip_site):
+        self.email = zulip_email
+        self.site = zulip_site
+        self.api = zulip_api
+        self.client = zulip.Client(
+            email=self.email, api_key=self.api, site=self.site)
+
+    def respond(self, msg):
+        content = msg['content'].strip().encode('utf-8')
+        print content
+
+    def send_message(self, content, type, to, subject):
+        response = self.client.send_message({
+            "type": type,
+            "to"	: to,
+            "subject"	: subject,
+            "content"	: content
+        })
+        print response
+
+    def main(self):
+        self.send_message("Hello", "stream", "bot-testing", "Subject")
+        self.client.call_on_each_event(
+            lambda msg: sys.stdout.write(str(msg) + "\n"))
+        self.client.call_on_each_message(self.respond)
+
+if __name__ == "__main__":
+    zulip_api = env.ZULIP_API_KEY
+    zulip_email = env.ZULIP_EMAIL
+    zulip_site = env.ZULIP_SITE
+    bot = cobotZulip(zulip_api, zulip_email, zulip_site)
+    bot.main()
